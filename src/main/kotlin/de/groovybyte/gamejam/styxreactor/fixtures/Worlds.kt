ad9a7e96f561f6458e50ba12ec2d658c1f69b22e
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2020-03-01 03:34:36 +0100

    add Hyperforge map and map cache

--- src/main/kotlin/de/groovybyte/gamejam/styxreactor/fixtures/Worlds.kt
+++ src/main/kotlin/de/groovybyte/gamejam/styxreactor/fixtures/Worlds.kt
@@ -5,13 +5,18 @@ import de.groovybyte.gamejam.styxreactor.game.world.Field
 import de.groovybyte.gamejam.styxreactor.game.world.World
 import kotlin.random.Random
 
-val WORLD_1 = World.generate(10, 10) { x, y ->
-    when(Random.nextInt(10)) {
-        in 0..1 -> Field.WallField.DEFAULT
-        in 1..2 -> Field.HoleField.DEFAULT
-        in 2..4 -> Field.AccessibleField.STEALTH
-        else -> Field.AccessibleField.DEFAULT
+object Worlds {
+    fun random() = World.generate(10, 10) { x, y ->
+        when(Random.nextInt(10)) {
+            in 0..1 -> Field.WallField.DEFAULT
+            in 1..2 -> Field.HoleField.DEFAULT
+            in 2..4 -> Field.AccessibleField.STEALTH
+            else -> Field.AccessibleField.DEFAULT
+        }
+    }
+
+    private val worldCache: MutableMap<String, World> = HashMap()
+    operator fun get(name: String): World = worldCache.getOrPut(name) {
+        World.load(jacksonObjectMapper(), "resources/worlds/$name.json")
     }
 }
-
-val WORLD_2 = World.load(jacksonObjectMapper(), "resources/worlds/Cloudspire.json")
--- src/main/kotlin/de/groovybyte/gamejam/styxreactor/game/world/WorldController.kt
+++ src/main/kotlin/de/groovybyte/gamejam/styxreactor/game/world/WorldController.kt
@@ -1,12 +1,11 @@
 package de.groovybyte.gamejam.styxreactor.game.world
 
+import de.groovybyte.gamejam.styxreactor.fixtures.Worlds
 import de.groovybyte.gamejam.styxreactor.game.datatransfer.server2client.ServerMessage
-import de.groovybyte.gamejam.styxreactor.fixtures.WORLD_1
-import de.groovybyte.gamejam.styxreactor.fixtures.WORLD_2
 import de.groovybyte.gamejam.styxreactor.framework.ClientContext
 
 object WorldController {
     fun sendWorld(ctx: ClientContext) {
-        ctx.send(ServerMessage("set_world", WORLD_2))
+        ctx.send(ServerMessage("set_world", Worlds["Hyperforge"]))
     }
 }
